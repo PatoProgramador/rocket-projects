f251474f1a49f90e0d9e5e5acdc54947f46d59c5
PatoProgramador <spartan1171947@hotmail.com>
2023-05-04 10:53:25 -0500

    project model

--- api/src/models/project.js
+++ api/src/models/project.js
@@ -1,1 +1,31 @@
 import { Schema, model } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const projectSchema = new Schema(
+    {
+        clientName: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: Number,
+            required: true
+        },
+        department: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+projectSchema.plugin(mongoosePaginate);
+
+export default model('users_test_kevinPatiño', projectSchema);
